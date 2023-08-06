60e62ea4f3814275f132ec1c4813771ffb6eb7f2
Juan Sebastian Fernandez Saez <jsebas1198@protonmail.com>
2023-08-06 18:55:55 -0400

    Reactividad en Angular

--- src/app/components/nav/nav.component.ts
+++ src/app/components/nav/nav.component.ts
@@ -1,13 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { StoreService } from 'src/app/services/store.service';
 
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
   styleUrls: ['./nav.component.scss'],
 })
-export class NavComponent {
+export class NavComponent implements OnInit {
+  //Contador para explicar la reactividad
+  counter = 0;
+
   //Estado que controla si se muestra o no el menú
   activeMenu = false;
+
+  constructor(private storeService: StoreService) {}
+  ngOnInit(): void {
+    this.storeService.myCart$.subscribe((products) => {
+      this.counter = products.length;
+    });
+  }
+
   //Funcion que modifica el estado para mostrar o no el menú en celulares
   toggleMenu() {
     this.activeMenu = !this.activeMenu;
--- src/app/services/store.service.ts
+++ src/app/services/store.service.ts
@@ -1,15 +1,27 @@
 import { Injectable } from '@angular/core';
-import { IProduct } from '../models/product.model';
+import { BehaviorSubject } from 'rxjs';
 
+import { IProduct } from '../models/product.model';
 @Injectable({
   providedIn: 'root',
 })
 export class StoreService {
   private myShoppingCart: IProduct[] = [];
+
+  //Tiene un array de productos que es lo que queremos transmitir
+  private myCart = new BehaviorSubject<IProduct[]>([]);
+
+  //Es el suscriptor de myCart, como si estuvieras suscripto a un canal de YouTube
+  //Es un  observable por eso tiene un $ por  buenas prácticas
+  //Un observable permite esuchar activamente cambios
+  myCart$ = this.myCart.asObservable();
+
   constructor() {}
 
   addProduct(product: IProduct) {
     this.myShoppingCart.push(product);
+//Con  el next emitimos un valor que seria  el estado de la lista
+    this.myCart.next(this.myShoppingCart);
   }
 
   getShoppingCart() {
