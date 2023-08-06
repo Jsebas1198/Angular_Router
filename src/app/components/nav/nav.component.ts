1e368b926799c0b9f68101bdf89e1164b7f3f4c7
Juan Sebastian Fernandez Saez <jsebas1198@protonmail.com>
2023-08-06 15:06:15 -0400

    Menu responsive

--- src/app/components/nav/nav.component.ts
+++ src/app/components/nav/nav.component.ts
@@ -3,8 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
-  styleUrls: ['./nav.component.scss']
+  styleUrls: ['./nav.component.scss'],
 })
 export class NavComponent {
-
+  //Estado que controla si se muestra o no el menú
+  activeMenu = false;
+  //Funcion que modifica el estado para mostrar o no el menú en celulares
+  toggleMenu() {
+    this.activeMenu = !this.activeMenu;
+  }
 }
