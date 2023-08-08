5adbcb9259516c863c727b02756a0f49cac6aecb
Juan Sebastian Fernandez <juan.fernandez@itti.digital>
2023-08-08 14:45:47 -0600

    Creación de un interceptor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { register } from 'swiper/element/bundle';
 import { SweetAlert2Module } from '@sweetalert2/ngx-sweetalert2';
 
@@ -14,6 +14,7 @@ import { NavComponent } from './components/nav/nav.component';
 import { ReversePipe } from './pipes/reverse.pipe';
 import { TimeAgoPipe } from './pipes/time-ago.pipe';
 import { HighlightDirective } from './directives/highlight.directive';
+import { TimeInterceptor } from './interceptors/time.interceptor';
 
 register();
 
@@ -35,7 +36,9 @@ register();
     HttpClientModule,
     SweetAlert2Module.forRoot(),
   ],
-  providers: [],
+  providers: [
+    { provide: HTTP_INTERCEPTORS, useClass: TimeInterceptor, multi: true },
+  ],
   bootstrap: [AppComponent],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
