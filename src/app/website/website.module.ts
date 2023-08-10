59bfd31081e936b8842e9f805af6676ad5dd56c7
Juan Sebastian Fernandez <juan.fernandez@itti.digital>
2023-08-10 14:20:56 -0600

    Creamos el módulo para la página principal

---
+++ src/app/website/website.module.ts
@@ -0,0 +1,48 @@
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SweetAlert2Module } from '@sweetalert2/ngx-sweetalert2';
+import { WebsiteRoutingModule } from './website-routing.module';
+import { register } from 'swiper/element/bundle';
+
+import { ImgComponent } from './components/img/img.component';
+import { ProductComponent } from './components/product/product.component';
+import { ProductsComponent } from './components/products/products.component';
+import { NavComponent } from './components/nav/nav.component';
+import { ReversePipe } from './pipes/reverse.pipe';
+import { TimeAgoPipe } from './pipes/time-ago.pipe';
+import { HighlightDirective } from './directives/highlight.directive';
+import { HomeComponent } from './pages/home/home.component';
+import { CategoryComponent } from './pages/category/category.component';
+import { MycartComponent } from './pages/mycart/mycart.component';
+import { LoginComponent } from './pages/login/login.component';
+import { RegisterComponent } from './pages/register/register.component';
+import { RecoveryComponent } from './pages/recovery/recovery.component';
+import { ProfileComponent } from './pages/profile/profile.component';
+import { ProductDetailComponent } from './pages/product-detail/product-detail.component';
+import { LayoutComponent } from './components/layout/layout.component';
+
+register();
+
+@NgModule({
+  declarations: [
+    ImgComponent,
+    ProductComponent,
+    ProductsComponent,
+    NavComponent,
+    ReversePipe,
+    TimeAgoPipe,
+    HighlightDirective,
+    HomeComponent,
+    CategoryComponent,
+    MycartComponent,
+    LoginComponent,
+    RegisterComponent,
+    RecoveryComponent,
+    ProfileComponent,
+    ProductDetailComponent,
+    LayoutComponent,
+  ],
+  imports: [CommonModule, WebsiteRoutingModule, SweetAlert2Module],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
+})
+export class WebsiteModule {}
